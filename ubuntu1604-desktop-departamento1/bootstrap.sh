c88c550363cc9ac7e2dbcc2d75717e6b8836dcb5
jlinares-upf <jose.linares@upf.edu>
2017-10-05 14:11:26 +0200

    modify errors and rm firefox in bootstrap.sh and add sync folder in Vagrantfile to ubuntu1604-desktop-departamento1 template

--- ubuntu1604-desktop-departamento1/bootstrap.sh
+++ ubuntu1604-desktop-departamento1/bootstrap.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-apt-get install -y firefox
+
 add-apt-repository ppa:alessandro-strada/google-drive-ocamlfuse-beta
 apt-get update
-apt-get install google-drive-ocamlfuse
+apt-get -y install google-drive-ocamlfuse
 mkdir ~/google-drive
-google-drive-ocamlfuse ~/google-drive
-fusermount -u ~/google-drive
+
+#google-drive-ocamlfuse ~/google-drive
+#fusermount -u ~/google-drive
